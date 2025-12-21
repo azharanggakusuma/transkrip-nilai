3b6765e3766b52aae5ab4a7b5129be98dae5095d
azharanggakusuma <azharanggakusuma01@gmail.com>
2025-12-21 22:01:05 +0700

    main: Fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 export default function Footer() {
   return (
+    // Font Cambria tetap dipertahankan
     <div className="flex justify-between items-start mt-6 text-[10px] w-full font-['Cambria']">
+      
       {/* Keterangan Kiri */}
       <div className="mt-0 pl-1">
         <p className="font-bold underline mb-1">Keterangan</p>
-        <div className="grid grid-cols-[30px_10px_1fr] leading-tight">
+        
+        {/* PERUBAHAN: grid-cols-[70px_...] disesuaikan menjadi 70px */}
+        <div className="grid grid-cols-[70px_10px_1fr] leading-tight">
           <div>SMT</div> <div>:</div> <div>Semester</div>
           <div>SKS</div> <div>:</div> <div>Satuan Kredit Semester</div>
           <div>HM</div> <div>:</div> <div>Huruf Mutu</div>
@@ -15,24 +19,21 @@ export default function Footer() {
         </div>
       </div>
 
-      {/* Area Tanda Tangan Kanan */}
+      {/* Area Tanda Tangan Kanan (Tetap sama) */}
       <div className="flex flex-col items-center w-fit mt-0 pr-1">
         <p className="mb-0 leading-tight">Cirebon, 21 Oktober</p>
-        <p className="font-normal mb-1 leading-tight">
+        <p className="font-normal leading-tight">
           Ketua Program Studi Teknik Informatika (S1)
         </p>
 
-        {/* CONTAINER VISUAL (TANPA STEMPEL, HANYA GAMBAR TTD) */}
         <div className="relative w-32 h-24 my-1 flex items-center justify-center">
-          {/* TANDA TANGAN (Image File) */}
           <img
             src="/img/ttd-kaprodi.png"
             alt="Tanda Tangan"
-            className="absolute w-full h-full object-contain z-10 top-0 left-0 mix-blend-multiply scale-[1.6] translate-y-[-20px]"
+            className="absolute w-full h-full object-contain z-10 top-0 left-0 mix-blend-multiply scale-[1.5] translate-y-[-20px]"
           />
         </div>
 
-        {/* NAMA */}
         <div className="text-center z-20 mt-[-35px] relative">
           <p className="font-bold underline text-[11px] leading-none">
             YUDHISTIRA ARIE WIJAYA, M.Kom
